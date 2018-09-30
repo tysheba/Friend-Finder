9b25c1e4f21fd7df5dd7675b4626f7707a499320
Tysheba <tmorgan2dev@gmail.com>
2018-09-30 14:06:48 -0400

    file setup

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,20 @@
+// display a JSON of all possible friends
+app.get("/api/friends", function (req, res) {
+    return res.json(friends);
+});
+
+// This will be used to handle incoming survey results. 
+// This route will also be used to handle the compatibility logic
+app.post("/api/friends", function (req, res) {
+
+    var newfriend = req.body; //?
+
+    console.log(newfriend);
+
+    // We then add the json the user sent to the friends array
+    friends.push(newfriend);
+
+    // We then display the JSON to the users  ?
+    res.json(newfriend);
+
+});
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,12 @@
+// A GET Route to `/survey` which should display the survey page
+
+app.get("/survey", function(req, res) {
+    
+    res.sendFile(path.join(__dirname, "survey.html"));
+  });
+
+// A default, catch-all route that leads to `home.html` which displays the home page. 
+app.get("/", function(req, res) {
+    
+    res.sendFile(path.join(__dirname, "home.html"));
+  });
